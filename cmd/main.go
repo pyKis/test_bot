e07b0738eb5966680da594c59652da5580b61116
pyKis <pavka.kis@yandex.ru>
2024-09-06 15:01:09 +0500

    env

--- cmd/main.go
+++ cmd/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	s "github.com/pyKis/test_bot/pkg/system"
+	"github.com/pyKis/test_bot/pkg/system"
 )
 
 
 func main(){
-	fmt.Println(s.BotToken)
+	fmt.Println(system.BotToken)
 }
